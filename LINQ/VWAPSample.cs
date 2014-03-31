f84f6105d75e6be17f2a7f374f7d75e50d2d9496
Pragmateek <postmaster@pragmateek.com>
2014-03-31 15:48:02 +0200

    Fix file hierarchy + start LINQ samples

---
+++ LINQ/VWAPSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class TradeLine
+    {
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class VWAPSample
+    {
+        public void Run()
+        {
+            TradeLine[] lines =
+            {
+
+            };
+
+            decimal VWAP = lines.Aggregate(0m, (sumProduct, line) => sumProduct + line.Price * line.Quantity) / lines.Sum(line => line.Quantity);
+
+            Console.WriteLine("VWAP: {0}.", VWAP);
+        }
+    }
+}
