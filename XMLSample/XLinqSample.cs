6dcab5475d7b933e4037a4e64aa5db15900c79a2
Pragmateek <postmaster@pragmateek.com>
2014-06-23 00:09:29 +0200

    XLINQ and XML data binding samples

---
+++ XMLSample/XLinqSample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLSample
+{
+    public class XLinqSample
+    {
+        public void Run()
+        {
+            XDocument document = XDocument.Load("configuration.xml");
+            Console.WriteLine(document.Root.Name);
+            foreach (XElement element in document.Root.Element("SubConfigurations").Elements())
+            {
+                XAttribute timestamp = element.Attribute("Timestamp");
+
+                Console.WriteLine("\t{0} ({1})", element.Name, timestamp != null ? timestamp.Value : "-");
+
+                if (element.Name == "DBConfiguration")
+                {
+                    Console.WriteLine("\t\t{0}", element.Element("DBServerHost").Value);
+                }
+            }
+        }
+    }
+}
---
+++ XMLSample/XMLDataBindingSample.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLSample
+{
+    [XmlRoot]
+    public class Configuration
+    {
+        [XmlArrayItem(typeof(UIConfiguration))]
+        [XmlArrayItem(typeof(NetworkConfiguration))]
+        [XmlArrayItem(typeof(DBConfiguration))]
+        public List<SubConfiguration> SubConfigurations { get; set; }
+    }
+
+    [XmlInclude(typeof(UIConfiguration))]
+    [XmlInclude(typeof(NetworkConfiguration))]
+    [XmlInclude(typeof(DBConfiguration))]
+    public class SubConfiguration
+    {
+        //[XmlAttribute]
+        public DateTime? Timestamp { get; set; }
+    }
+
+    public class UIConfiguration : SubConfiguration
+    {
+        public bool IsFullScreen { get; set; }
+    }
+
+    public class NetworkConfiguration : SubConfiguration
+    {
+        public string ServerAddress { get; set; }
+    }
+
+    public class DBConfiguration : SubConfiguration
+    {
+        public string DBServerHost { get; set; }
+        public string DBServerSchema { get; set; }
+    }
+
+    public class XMLDataBindingSample
+    {
+        public void Run()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+
+            Configuration config = null;
+            using (FileStream stream = File.OpenRead("configuration.xml"))
+            {
+                config = (Configuration)serializer.Deserialize(stream);
+            }
+
+            UIConfiguration UIConfig = (UIConfiguration)config.SubConfigurations[0];
+            UIConfig.IsFullScreen = !UIConfig.IsFullScreen;
+
+            using (FileStream stream = File.OpenWrite("configuration.xml"))
+            {
+                serializer.Serialize(stream, config);
+            }
+        }
+    }
+}
