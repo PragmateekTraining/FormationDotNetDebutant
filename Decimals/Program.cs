74d37e96c917c61e7df80aa339da1191594b6664
Pragmateek <training@pragmateek.com>
2014-10-13 19:12:15 +0200

    Add logical operators sample to demonstrate short-circuiting

--- Decimals/Program.cs
+++ Decimals/Program.cs
@@ -6,13 +6,13 @@ namespace Decimals
     {
         static void Main(string[] args)
         {
-            /*for (double x = 0; x != 10.0; x += 0.2)
+            /*for (float x = 0; x != 10.0; x += 0.2)
             {
                 Console.Write(x);
                 Console.ReadLine();
             }*/
 
-            for (double x = 0; x != 10.0; x += 0.2)
+            for (float x = 0; x != 10.0; x += 0.2f)
             {
                 Console.Write(DoubleConverter.ToExactString(x));
                 Console.ReadLine();
---
+++ LogicalOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static bool True1()
+        {
+            Console.WriteLine("True1()");
+
+            return true;
+        }
+
+        static bool False()
+        {
+            Console.WriteLine("False()");
+
+            return false;
+        }
+
+        static bool True2()
+        {
+            Console.WriteLine("True2()");
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Using & operators ===");
+
+            if (True1() & False() & True2()) ;
+
+            Console.WriteLine("=== Using && operators ===");
+
+            if (True1() && False() && True2()) ;
+        }
+    }
+}
